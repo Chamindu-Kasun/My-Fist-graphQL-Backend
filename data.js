555d830adb69cbc9ffe0c08f7bbab7ca6409f8f8
Chamindu-Kasun <66759078+Chamindu-Kasun@users.noreply.github.com>
2022-01-13 15:52:00 +0530

    Initial Commit

---
+++ data.js
@@ -0,0 +1,13 @@
+const authors = [
+    {id:1 , name:"J. K. Rowling"},
+    {id:2 , name:"J. R. R. Tolkien"},
+    {id:3 , name:"Brent Weeks"}
+]
+
+const books = [
+    {id:1,name:"Harry Potter",authorId:1},
+    {id:2,name:"The fellowship of the ring",authorId:2},
+    {id:3,name:"The way of shadows",authorId:3},
+]
+
+module.exports = { authors,books }
